5f75a72ebbb85ad0d080112b78b23350abe041bc
akalanka <himesha@outlook.com>
2020-08-13 13:39:35 +0530

    ratings and review prototype

--- sewaseven/app/src/main/java/com/sewaseven/sewaseven/ServicePage.java
+++ sewaseven/app/src/main/java/com/sewaseven/sewaseven/ServicePage.java
@@ -1,14 +1,66 @@
 package com.sewaseven.sewaseven;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class ServicePage extends AppCompatActivity {
+
+
+    private ImageButton makeCallToService;
+    private ImageButton ratingsAndReviews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_page);
+
+        makeCallToService = findViewById(R.id.btnMakeCall);
+        makeCallToService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                if (ActivityCompat.checkSelfPermission(Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED)
+//                   { makeCall();}
+
+            }
+        });
+
+        ratingsAndReviews = findViewById(R.id.btnAddRatings);
+        ratingsAndReviews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ratingsAndReviewsIntent = new Intent(ServicePage.this,RatingsAndReviews.class);
+                startActivity(ratingsAndReviewsIntent);
+
+            }
+        });
+    }
+
+
+
+
+    protected void makeCall() {
+
+
+        String d = "tel:" + "0772637357" ;
+
+        Intent phoneIntent = new Intent(Intent.ACTION_CALL);
+        phoneIntent.setData(Uri.parse(d));
+        try {
+            startActivity(phoneIntent);
+            finish();
+
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "Call faild, please try again later.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
