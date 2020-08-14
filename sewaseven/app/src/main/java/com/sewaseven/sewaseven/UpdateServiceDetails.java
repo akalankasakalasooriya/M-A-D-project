1696faf2eb262d4be57d36f6c015c426cc556198
kanchilalakindu <kanchilalakindu@gmail.com>
2020-08-14 22:24:45 +0530

    Done UpdateServiceDetails and deleteservice

--- sewaseven/app/src/main/java/com/sewaseven/sewaseven/UpdateServiceDetails.java
+++ sewaseven/app/src/main/java/com/sewaseven/sewaseven/UpdateServiceDetails.java
@@ -3,12 +3,34 @@ package com.sewaseven.sewaseven;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class UpdateServiceDetails extends AppCompatActivity {
+public class UpdateServiceDetails extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_service_details);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.location, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
