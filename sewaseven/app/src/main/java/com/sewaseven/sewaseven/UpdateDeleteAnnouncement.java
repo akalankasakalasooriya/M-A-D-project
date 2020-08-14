6799b78adb875074336715b2e191004f752699a6
Reshika-crypto <reshikarajapaksha9@gmail.com>
2020-08-14 09:59:18 +0530

    NewAnnouncement,Previous Announcement,UpdateDelete Announcements -Reshika

---
+++ sewaseven/app/src/main/java/com/sewaseven/sewaseven/UpdateDeleteAnnouncement.java
@@ -0,0 +1,14 @@
+package com.sewaseven.sewaseven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateDeleteAnnouncement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_delete_announcement);
+    }
+}
