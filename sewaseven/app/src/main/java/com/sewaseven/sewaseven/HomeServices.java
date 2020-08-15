cc66d04fd3465f685e12c9c26b81124990fcec02
akalanka <himesha@outlook.com>
2020-08-15 11:08:10 +0530

    prototyping finished

--- sewaseven/app/src/main/java/com/sewaseven/sewaseven/HomeServices.java
+++ sewaseven/app/src/main/java/com/sewaseven/sewaseven/HomeServices.java
@@ -1,9 +1,11 @@
 package com.sewaseven.sewaseven;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
@@ -23,5 +25,23 @@ public class HomeServices extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_home_services, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        LinearLayout btnServiceProfileS;
+
+        btnServiceProfileS = getView().findViewById(R.id.linktoProfileS);
+        btnServiceProfileS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent servicePage = new Intent(getActivity(),ServicePage.class);
+                startActivity(servicePage);
+            }
+        });
     }
 }
