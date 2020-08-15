c101e86b07b305ad25be81a93ba57f3f0c048bc2
akalanka <himesha@outlook.com>
2020-08-15 20:21:28 +0530

    xml update

--- sewaseven/app/src/main/java/com/sewaseven/sewaseven/PreviousAnnouncement.java
+++ sewaseven/app/src/main/java/com/sewaseven/sewaseven/PreviousAnnouncement.java
@@ -6,9 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class PreviousAnnouncement extends AppCompatActivity {
-    ImageButton updateDeleteAnnouncement;
+    TextView updateDeleteAnnouncement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
